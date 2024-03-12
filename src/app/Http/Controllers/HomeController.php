e5e21b74f47f07d76e203bf26fcbdddd515dca4e
sugiura <sugiura@winas.jp>
2024-03-12 09:14:10 +0900

    Memo追加 php artisan make:model Memo

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,48 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        #メモ一覧を取得する
+        $user = \Auth::user();
+        $memos = Memo::where('user_id', $user->id)->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('home', compact('user','memos'));
     }
+    public function create()
+    {
+        $user = \Auth::user();
+        $memos = Memo::where('status', 1)->where('user_id', $user['id'])->get();
+        return view('create', compact('user','memos'));
+    }
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        dd($data);
+        $memo_id = Memo::insertGetId([
+            'content' => $data['content'],
+            'user_id' => $data['user_id'],
+            //'tag' => $data['tag'],
+            'status' => 1
+        ]);
+        /*
+        $memo = new Memo;
+        $memo->user_id = $request->user_id;
+        $memo->content = $request->content;
+        //$memo->tag = $request->tag;
+        $memo->status = 1;
+        $memo->save();
+        */
+        return redirect('/')->with('success', 'メモを作成しました');
+    }
+    public function edit($id){
+        $user = \Auth::user();
+        $memo  = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])->first();
+        $memos = Memo::where('status', 1)->where('user_id', $user['id'])->get();
+        return view('edit', compact('memos','memo', 'user'));
+    }
+    public function update(Request $request, $id){
+        $user = \Auth::user();
+
+        $inputs = $request->all();
+        Memo::where('id', $id)->update(['content' => $inputs['content']]);
+        return redirect()->route('home');
+    }    
 }
